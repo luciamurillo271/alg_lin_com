1a20c7fbe0e1347e1319279329edd50633483779
Lucia Murillo <luciamurillo271@gmail.com>
2026-08-21 15:29:26 -0300

    matrices iguales modulo

---
+++ laboratorio2/modulo.py
@@ -0,0 +1,18 @@
+import numpy as np
+def error(x,y):
+    return np.float64(x) - np.float64(y)
+'''Recibe dos numeros x e y, y calcula el error de aproximar x usando y en float64'''
+
+def error_relativo(x,y):
+    return
+'''Recibe dos numeros x e y, y calcula el error relativo de aproximar x usando y en float64'''
+    
+def matricesIguales(A, B):
+    iguales = True
+    if len(A) != len(B) or len(A[0]) != len(B[0]):
+        iguales = False
+    else:
+        iguales =A.all() == B.all()
+        #for i in range(len(A)):
+    return iguales
+'''Devuelve True si ambas matrices son iguales y False en otro caso. Considerar que las matrices pueden tener distintas dimensiones, ademas de distintos valores.'''
